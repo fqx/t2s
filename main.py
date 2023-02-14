49626106b0910df0c296a230777f255befa1a1b4
fqx <qfuxiang@gmail.com>
2023-02-14 19:00:11 +0800

    add txt support

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # This is a Python script to convert Traditional Chinese to Simplified Chinese of srt file.
-import opencc, sys
-
+import opencc, sys, re
+re_srt = re.compile('srt$')
 
 if __name__ == '__main__':
     converter = opencc.OpenCC('t2s')
     filename = sys.argv[1]
     new_file = []
-    with open(filename, 'r') as f:
-        content = f.readlines()
+    is_srt = False
+    if re_srt.search(filename):
+        is_srt = True
+    if is_srt:
+        with open(filename, 'r') as f:
+            content = f.readlines()
+    else: # txt
+        with open(filename, 'r') as f:
+            content = f.read().split()
     i = 1
     for line in content:
-        if i % 4 == 3:
+        if (not is_srt) or i % 4 == 3:
             line = converter.convert(line)
+            if not is_srt:
+                line += '\n'
         new_file.append(line)
         i += 1
     with open(filename, 'w') as f:
